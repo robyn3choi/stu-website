0b052bf4861e59926992eeba9da40de1f2c5472b
robyn <robyn3choi@gmail.com>
2019-02-26 17:37:41 -0800

    rough version of nav animation

--- src/Home/Header/Header.js
+++ src/Home/Header/Header.js
@@ -1,16 +1,75 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Header.scss';
 import HeaderCanvas from './HeaderCanvas/HeaderCanvas';
+import { Route, Link, Switch, BrowserRouter as Router } from 'react-router-dom';
+import { HashLink } from 'react-router-hash-link';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import About from './../../About/About';
+import Contact from './../../Contact/Contact';
 
-const Header = (props) => {
-  return (
-    <div className='header'>
-      <HeaderCanvas position='back'/>
-      <HeaderCanvas position='front'/>
-      <img className='header__title' alt='header-title' src={require('./stu-title.png')} />
-      <img className='header__face' alt='header' src={require('./stu-face.png')} />
-    </div>
-  );
+class Header extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      hoveredElementPos: null
+    }
+    this.aboutRef = React.createRef();
+    this.musicRef = React.createRef();
+    this.contactRef = React.createRef();
+  }
+
+  onNavMouseEnter(link) {
+    if (link === 'about') {
+      const rect = this.aboutRef.current.getBoundingClientRect();
+      this.setState({hoveredElementPos: {x: rect.x, y: rect.y}});
+    }
+  }
+
+  onNavMouseLeave(link) {
+    console.log("leave");
+    this.setState({hoveredElementPos: null});
+  }
+
+  render() {
+    return (
+      <div className='header'>
+        <Router>
+
+          <div className='nav'>
+            <div className='nav__link-container nav__link-container_about' ref={this.aboutRef} 
+              onMouseEnter={() => this.onNavMouseEnter('about')} onMouseLeave={() => this.onNavMouseLeave('about')}>
+              <Link className='nav__link' to="/about">About</Link>
+            </div>
+            <div className='nav__link-container nav__link-container_music' ref={this.musicRef} 
+              onMouseEnter={() => this.onNavMouseEnter('music')} onMouseLeave={() => this.onNavMouseLeave('music')}>
+              <HashLink smooth to="/#music" className='nav__link'>Music</HashLink>
+            </div>
+            <div className='nav__link-container nav__link-container_contact' ref={this.contactRef} 
+              onMouseEnter={() => this.onNavMouseEnter('contact')} onMouseLeave={() => this.onNavMouseLeave('contact')}>
+              <Link to="/contact" className='nav__link'>Contact</Link>
+            </div>
+
+            <TransitionGroup component={null}>
+              <Route render={location => {
+                return (
+                  <Switch>
+                    <Route path='/about' render={props => <About {...props} paragraphs={props.aboutParagraphs} />} />
+                    <Route path='/contact' render={props => <Contact {...props} email={props.contactEmail} />} />
+                  </Switch>
+                )
+              }}
+              />
+            </TransitionGroup>
+          </div>
+        </Router>
+        <HeaderCanvas position='back' hoveredElementPos={this.state.hoveredElementPos} />
+        <HeaderCanvas position='front' hoveredElementPos={this.state.hoveredElementPos} />
+        <img className='header__title' alt='header-title' src={require('./stu-title.png')} />
+        <img className='header__face' alt='header' src={require('./stu-face.png')} />
+      </div>
+    );
+  }
 }
 
 
--- src/Home/Header/HeaderCanvas/HeaderCanvas.js
+++ src/Home/Header/HeaderCanvas/HeaderCanvas.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Triangle from './Triangle';
 import './HeaderCanvas.scss';
 
@@ -8,21 +8,25 @@ class HeaderCanvas extends Component {
     this.canvasRef = React.createRef();
     this.ctx = {};
     this.triangles = [];
+    this.floatingTweens = [];
+    this.homingTweens = [];
+    this.spreadingTweens = [];
+    this.floatingPositions = [];
   }
-  
+
   componentDidMount() {
     const canvas = this.canvasRef.current;
-    canvas.width = window.innerWidth +100;
+    canvas.width = window.innerWidth + 100;
     canvas.height = window.innerHeight;
     this.ctx = canvas.getContext('2d');
-    const numTriangles = canvas.width * canvas.height/15000;
-    console.log(numTriangles);
-    for(var x = 0; x < numTriangles; x++) {
+    const numTriangles = canvas.width * canvas.height / 15000;
+
+    for (var x = 0; x < numTriangles; x++) {
       const t = new Triangle(canvas.width, canvas.height);
       this.triangles.push(t);
-      this.tweenTriangle(t, x);
+      this.floatingTweens.push(this.floatingTween(t, x));
     }
-    
+
     this.animate();
   }
 
@@ -30,7 +34,7 @@ class HeaderCanvas extends Component {
     //this.ctx.clearRect(0,0,this.canvasRef.current.width,this.canvasRef.current.height);
     this.ctx.canvas.width = window.innerWidth + 100;
     this.ctx.canvas.height = window.innerHeight;
-    for(var i in this.triangles) {
+    for (var i in this.triangles) {
       this.drawTriangle(this.triangles[i]);
     }
     requestAnimationFrame(() => this.animate());
@@ -45,32 +49,79 @@ class HeaderCanvas extends Component {
     this.ctx.scale(triangle.scale, triangle.scale);
     this.ctx.translate(-x, -y);
     this.ctx.beginPath();
-    this.ctx.moveTo(triangle.coords[0].x+triangle.x, triangle.coords[0].y+triangle.y);
-    this.ctx.lineTo(triangle.coords[1].x+triangle.x, triangle.coords[1].y+triangle.y);
-    this.ctx.lineTo(triangle.coords[2].x+triangle.x, triangle.coords[2].y+triangle.y);
+    this.ctx.moveTo(triangle.coords[0].x + triangle.x, triangle.coords[0].y + triangle.y);
+    this.ctx.lineTo(triangle.coords[1].x + triangle.x, triangle.coords[1].y + triangle.y);
+    this.ctx.lineTo(triangle.coords[2].x + triangle.x, triangle.coords[2].y + triangle.y);
     this.ctx.closePath();
-    this.ctx.fillStyle = 'rgba('+triangle.color+','+ triangle.alpha+')';
+    this.ctx.fillStyle = 'rgba(' + triangle.color + ',' + triangle.alpha + ')';
     this.ctx.fill();
     this.ctx.restore();
   }
 
-  tweenTriangle(triangle, i) {
+  floatingTween(triangle, i) {
     const width = this.canvasRef.current.width;
     const dir = i % 2 === 0 ? 1 : -1;
-    const rotation = -20 + 40*Math.random();
-    var time = 100+300*Math.random();
-    window.TweenMax.to(triangle, time, 
+    const rotation = -20 + 40 * Math.random();
+    var time = 100 + 300 * Math.random();
+    return window.TweenMax.to(triangle, time,
       {
         rotation: rotation,
-        x: triangle.x + dir*(width), 
-        repeat:-1,
+        x: triangle.x + dir * (width),
+        repeat: -1,
         yoyo: true
       });
   }
 
+  componentDidUpdate(prevProps) {
+    if (prevProps === this.props) return;
+
+    //window.TweenMax.killAll();
+    this.floatingTweens.length = 0;
+    this.spreadingTweens.length = 0;
+    this.homingTweens.length = 0;
+    console.log(this.props);
+    // we've started hovering over a link
+    if (this.props.hoveredElementPos) {
+      for (const triangle of this.triangles) {
+        this.floatingPositions.push({ x: triangle.x, y: triangle.y });
+        this.homingTweens.push(this.homingTween(triangle));
+      }
+    }
+    // we've stopped hovering over a link
+    else {
+      console.log(this.floatingPositions);
+      for (const [i, triangle] of this.triangles.entries()) {
+        this.spreadingTweens.push(this.spreadingTween(triangle, i));
+      }
+    }
+  }
+
+  spreadingTween(triangle, i) {
+    return window.TweenMax.to(triangle, 1,
+      {
+        x: this.floatingPositions[i].x,
+        y: this.floatingPositions[i].y,
+        //ease: window.Power3.easeOut,
+        onComplete: () => {
+          this.floatingTweens.push(this.floatingTween(triangle, i));
+        }
+      });
+  }
+
+  homingTween(triangle) {
+
+    return window.TweenMax.to(triangle, 1,
+      {
+        x: this.props.hoveredElementPos.x,
+        y: this.props.hoveredElementPos.y,
+        ease: window.Power3.easeOut
+      });
+  }
+
+
   render() {
     return (
-        <canvas className={`header__canvas header__canvas_${this.props.position}`}  ref={this.canvasRef} />
+      <canvas className={`header__canvas header__canvas_${this.props.position}`} ref={this.canvasRef} />
     );
   }
 }
