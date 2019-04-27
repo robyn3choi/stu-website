3ffdb918db8531cc3ef32cedb9aeea711b6365e7
robyn <robyn3choi@gmail.com>
2019-04-26 19:57:16 -0700

    trying different name for highlighted music item icon

--- src/Music/MusicItemIcon/MusicItemIcon.js
+++ src/Music/MusicItemIcon/MusicItemIcon.js
@@ -6,7 +6,7 @@ const MusicItemIcon = (props) => {
     <div className={`music-item__icon ${props.className}`}>
     <a href={props.url} target="_blank" rel="noopener noreferrer">
       <img className={`music-item__icon-image ${props.className}`} alt={props.platform} src={require(`./${props.platform}-icon.png`)} />
-      <img className={`music-item__icon-image music-item__icon-image_highlighted ${props.className}`} alt={`${props.platform}-highlighted`} src={require(`./${props.platform}-icon-highlighted.png`)} />
+      <img className={`music-item__icon-image music-item__icon-image_highlighted ${props.className}`} alt={`${props.platform}-highlighted`} src={require(`./${props.platform}-icon-blah.png`)} />
     </a>
     </div>
   )
